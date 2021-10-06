1cd7f0f0935f673d55cce52b7cecebfe1232b91e
marcosvandrade <vascorj2006@gmail.com>
2021-10-06 16:23:49 -0300

    first deploy contabo

--- js/cookies.js
+++ js/cookies.js
@@ -2,12 +2,12 @@ const cookieContainer = document.querySelector(".cookie-container");
 const cookieButton = document.querySelector(".cookie-btn");
 
 cookieButton.addEventListener("click", () => {
-  cookieContainer.classList.remove("active");
-  localStorage.setItem("cookieBannerDisplayed", "true");
+    cookieContainer.classList.remove("active");
+    localStorage.setItem("cookieBannerDisplayed", "true");
 });
 
 setTimeout(() => {
-  if (!localStorage.getItem("cookieBannerDisplayed")) {
-    cookieContainer.classList.add("active");
-  }
+    if (!localStorage.getItem("cookieBannerDisplayed")) {
+        cookieContainer.classList.add("active");
+    }
 }, 2000);
