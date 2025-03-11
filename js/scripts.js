0ef68a30c7587a5253ad265fe68d2a4e4b07ed96
marcosvandrade <vascorj2006@gmail.com>
2025-03-11 13:04:30 -0300

    adding blog link

--- js/scripts.js
+++ js/scripts.js
@@ -9,52 +9,28 @@
         // Treat as normal link if no-scroll class
         if ($(this).hasClass('no-scroll')) return;
 
-        e.preventDefault();
         var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
 
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+        // Check if the href is a valid CSS selector (starts with #)
+        if (heading.startsWith('#')) {
+            e.preventDefault(); // Prevent default behavior only for valid selectors
 
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
+            var scrollDistance = $(heading).offset().top;
+
+            $('html, body').animate({
+                scrollTop: scrollDistance + 'px'
+            }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+
+            // Hide the menu once clicked if mobile
+            if ($('header').hasClass('active')) {
+                $('header, body').removeClass('active');
+            }
         }
+        // If it's not a valid selector (e.g., a URL), allow the link to behave normally
     });
 
-    //Scroll to top
-    $('#to-top1').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
-    $('#to-top2').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
-    $('#to-top3').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
-    $('#to-top4').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
-    $('#to-top5').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
-    $('#to-top6').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
-    $('#to-top7').click(function() {
+    // Scroll to top
+    $('#to-top1, #to-top2, #to-top3, #to-top4, #to-top5, #to-top6, #to-top7').click(function() {
         $('html, body').animate({
             scrollTop: 0
         }, 500);
