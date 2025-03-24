395e59c798f896423dec5b0789b9e13a96b60c6a
marcosvandrade <vascorj2006@gmail.com>
2025-03-24 09:37:15 -0300

    adjusting hamburguer menu

--- js/hamburguer.js
+++ js/hamburguer.js
@@ -1,16 +1,3 @@
-// function clickMenu() {
-//     if (itens.style.display == 'block') {
-//         itens.style.display = 'none';
-//     } else {
-//         itens.style.display = 'block';
-//     }
-// }
-
 function clickMenu() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-    } else {
-        menuMobile.classList.add('open')
-    }
+    document.querySelector('.mobile-menu').classList.toggle('open');
 }
